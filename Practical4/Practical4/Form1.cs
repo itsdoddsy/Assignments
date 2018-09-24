acb3b4a4486aa07b483fa12ac40349e2034d46a0
itsdoddsy <23368872+itsdoddsy@users.noreply.github.com>
2018-09-24 02:34:33 +0100

    Create practical 4

---
+++ Practical4/Practical4/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practical4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal monthlyInvestment = Convert.ToDecimal(txtMonthlyInvestment.Text);
+                decimal yearlyInterestRate = Convert.ToDecimal(txtInterestRate.Text);
+                int years = Convert.ToInt32(txtYears.Text);
+                int months = years * 12;
+                decimal monthlyInterestRate = yearlyInterestRate / 12 / 100;
+                decimal futureValue = 0m;
+
+                for (int i = 0; i < months; i++)
+                    futureValue = (futureValue + monthlyInvestment) * (1 + monthlyInterestRate);
+
+                txtFutureValue.Text = futureValue.ToString("c");
+                txtMonthlyInvestment.Focus();
+            }
+            catch(Exception)
+            {
+                txtFutureValue.Text = "An error occured";
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
